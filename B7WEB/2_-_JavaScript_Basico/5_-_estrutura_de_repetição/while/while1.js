a9def2546667f0cd17df2e2d335a42bb08e72753
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-01 12:23:39 -0300

    Adicionando o código exemplo de while

---
+++ B7WEB/2_-_JavaScript_Basico/5_-_estrutura_de_repetição/while/while1.js
@@ -0,0 +1,7 @@
+let num = 1;
+
+while(num <= 10) {
+    console.log(`O número da vez é ${num}`)
+    num++;  // É recomendado utilizar essa incremento da variável 'num' para evitar que
+            // o while possa repetir o comando infinitamente
+}
