1575831bc52c27e92517dd764385f6a054accda8
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-01 12:37:40 -0300

    Alterando o nome para uma questão de exercício

---
+++ B7WEB/2_-_JavaScript_Basico/atividades/atividade4_loop_parte1.js
@@ -0,0 +1,6 @@
+//1. Faça um loop que mostre todas as frutas
+let frutas = ['Pêra', 'Uva', 'Maçã', 'Banana']
+console.log("A lista de frutas:")
+for(let fruta of frutas) {
+    console.log(`\t* ${fruta}`)
+}
