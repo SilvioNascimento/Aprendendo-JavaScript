041938a85289cc054eb8201553fbc81e746426f0
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-07-29 14:17:00 -0300

    feat: adicionar aulas de b7web

---
+++ B7WEB/3_-_Eventos_e_Manipulações_em_DOM/1_-_Selecionando_elemento/Exercicio1_Selecao_de_Elementos/script.js
@@ -0,0 +1,8 @@
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+let id = document.getElementById("d1");
+console.log(id);
+
+let classe = document.getElementsByClassName("saudacao");
+console.log(classe);
